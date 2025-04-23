607ca386f33933703333cb90c12d4624d928c239
Ismail Hatti <ihatti@bordeaux-inp.fr>
2025-04-23 17:19:39 +0200

    change again

--- src/components/GameControls.vue
+++ src/components/GameControls.vue
@@ -18,7 +18,7 @@ const game = useGameStore()
       Score diali
     </div>
     <div op50>
-      Join the tiles, get to 2048 you idiot!
+      Join the tiles, get to 2048 you idiotkkkkkk!
     </div>
   </div>
 
